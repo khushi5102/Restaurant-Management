ecb0c93e2c798bf2004200fedc44e4083249ddbc
khushi5102 <60965765+khushi5102@users.noreply.github.com>
2022-02-15 17:34:56 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		homepage
+	</title>
+</head>
+<body>
+    <link rel="stylesheet" type="text/css" href="indexs.css">
+<header>
+	<div class="title">
+		<h1>
+			CURATED CUISINES
+		</h1>
+        <h2>
+        -At Your Service
+        </h2>
+        <marquee scrollamount="9"><h4>Please select an option as per your designation</h4></marquee>
+	</div>
+	<div class="button">
+		<a href="customer/cust_home.php" class="btn">CUSTOMER</a>
+		<a href="worker/worker_login.php" class="btn">WORKER</a>
+		<a href="admin/admin_login.php" class="btn">ADMIN</a>
+	</div>
+</header>
+</body>
+</html>
